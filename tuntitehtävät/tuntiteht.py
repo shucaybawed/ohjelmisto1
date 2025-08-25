73c4116a8e4497fa70eede8be2252da38046b65c
lenovo <shubbe04@gmail.com>
2025-08-25 15:43:46 +0300

    tehtävät

---
+++ tuntitehtävät/tuntiteht.py
@@ -0,0 +1,5 @@
+vuosi = int(input("anna vuosilukuvuosi: "))
+ if vuosi % 4 == 0:
+    print ("oli olympia vuosi")
+else:
+    print ("ei ollut olympia vuosi")
---
+++ tuntitehtävät/tuntitehtävä1.py
@@ -0,0 +1,6 @@
+pituus = int(input("kuinka pitkä olet: "))
+if 100<=pituus<=140:
+    print("saa mennä lasten laitteisiin: ")
+elif pituus>=140:
+    print("saa mennä kaikkiin laitteisiin:")
+
